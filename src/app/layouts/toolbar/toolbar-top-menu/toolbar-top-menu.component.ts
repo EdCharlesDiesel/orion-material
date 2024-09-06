b164b0858e79f94901ec0c4d15c2d9be020a1ec5
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2024-09-06 17:10:56 +0200

    Navbat and side nav
    Added a toggling side bar

--- src/app/layouts/toolbar/toolbar-top-menu/toolbar-top-menu.component.ts
+++ src/app/layouts/toolbar/toolbar-top-menu/toolbar-top-menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-toolbar-top-menu',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar-top-menu.component.css']
 })
 export class ToolbarTopMenuComponent {
+  title = 'material-responsive-sidenav';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+  isCollapsed = true;
 
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches){
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  toggleMenu() {
+    if(this.isMobile){
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    } else {
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
